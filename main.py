ea010114a33db022291db12aa426474433265156
Antisocial-Gamer <james83701@gmail.com>
2024-01-29 12:22:42 -0500

    added function for moving of queens

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def chessBoardInit():
+import random
+def freshBoard():
     board = []
     for j in range(8):
         row = []
@@ -7,20 +8,30 @@ def chessBoardInit():
             row.append(0)
 
         board.append(row)
-
+    for i in range(8):
+        board[random.randint(0, 7)][i] = 1
     return board
 
-rows = 8
-columns = 8
-chessBoard = chessBoardInit()
-
-
-def printBoard(chessBoard):
+def printBoard(board):
     for x in range(rows):
         for y in range(columns):
-            print(chessBoard[x][y], end = '')
+            print(board[x][y], end = '')
             if y != 7:
                 print(", ", end = '')
         print()
 
+def moveQueen(board, column, newRow):
+    for i in range(8):
+        board[i][column] = 0
+    board[newRow][column] = 1
+    return board
+
+
+rows = 8
+columns = 8
+chessBoard = freshBoard()
 printBoard(chessBoard)
+
+
+
+
