ef10de646d4b0d5db0fb98a0874db569640c0f36
Antisocial-Gamer <james83701@gmail.com>
2024-01-29 18:29:15 -0500

    added heuristic counting and changed to OOP

---
+++ Board.py
@@ -0,0 +1,96 @@
+import random
+class Board:
+    # done up to counting heuristics
+    boardList = []
+    def __init__(self):
+        self.resetBoard()
+    
+    def resetBoard(self):
+        self.boardList = [[0 for i in range(8)] for j in range(8)]
+        
+        for column in range(8):
+            self.boardList[column][random.randint(0, 7)] = 1
+
+    def printBoard(self):
+        for row in range(8):
+            for column in range(8):
+                print(self.boardList[column][row], end = '')
+                if column != 7:
+                    print(", ", end = '')
+            print("")
+
+    def moveQueen(self, column, newRow):
+        for row in range(8):
+            self.boardList[column][row] = 0
+        self.boardList[column][newRow] = 1  
+
+    def checkRow(self, column, row):
+        conflict = set()
+        for checkColumn in range(8):
+            if (self.boardList[checkColumn][row] == 1) and column != checkColumn:
+                conflict.add(tuple([checkColumn, row]))
+                conflict.add(tuple([column, row]))
+                
+        return conflict
+
+    def countH(self):
+        queensInConflict = set()
+        for row in range(8):
+            for column in range(8):
+                if self.boardList[column][row] == 1:
+                    queensInConflict = queensInConflict.union(self.checkRow(column, row))
+                    queensInConflict = queensInConflict.union(self.checkDiagonal(column, row))
+        
+        return queensInConflict
+
+    def checkDiagonal(self, column, row):
+        conflict = set()
+        conflict = conflict.union(self.checkDiagonalNorthEast(column, row))
+        conflict = conflict.union(self.checkDiagonalNorthWest(column, row))
+        conflict = conflict.union(self.checkDiagonalSouthEast(column, row))
+        conflict = conflict.union(self.checkDiagonalSouthWest(column, row))
+        return conflict
+    
+    def checkDiagonalNorthEast(self, column, row):
+        conflict = set()
+        for i in range(1, 8):
+            
+            if column + i < 8 and row + i < 8:
+                if (self.boardList[column + i][row + i] == 1):
+                    conflict.add(tuple([column, row]))
+            else:
+                break
+
+        return conflict
+    
+    def checkDiagonalNorthWest(self, column, row): # col -, row +
+        conflict = set()
+        for i in range(1, 8):
+            if column - i > -1 and row + i < 8:
+                if (self.boardList[column - i][row + i] == 1):
+                    conflict.add(tuple([column, row]))
+            else:
+                break
+        return conflict
+    
+    def checkDiagonalSouthEast(self, column, row):# col +, row -
+        conflict = set()
+        for i in range(1, 8):
+            if column + i < 8 and row - i > -1:
+                if (self.boardList[column + i][row - i] == 1):
+                    conflict.add(tuple([column, row]))
+            else:
+                break
+        return conflict
+    
+    def checkDiagonalSouthWest(self, column, row):# col -, row -
+        conflict = set()
+        for i in range(1, 8):
+            
+            if column - i > -1 and row + i > -1:
+                if (self.boardList[column - i][row - i] == 1):
+                    conflict.add(tuple([column, row]))
+            else:
+                break
+        return conflict
+    
--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 import random
-def freshBoard():
-    board = []
-    for j in range(8):
-        row = []
+from Board import Board
 
-        for i in range(8):
-            row.append(0)
+chessBoard = Board()
+chessBoard.printBoard()
+print(len(chessBoard.countH()))
 
-        board.append(row)
-    for i in range(8):
-        board[random.randint(0, 7)][i] = 1
-    return board
-
-def printBoard(board):
-    for x in range(rows):
-        for y in range(columns):
-            print(board[x][y], end = '')
-            if y != 7:
-                print(", ", end = '')
-        print()
-
-def moveQueen(board, column, newRow):
-    for i in range(8):
-        board[i][column] = 0
-    board[newRow][column] = 1
-    return board
-
-
-rows = 8
-columns = 8
-chessBoard = freshBoard()
-printBoard(chessBoard)
 
 
 
